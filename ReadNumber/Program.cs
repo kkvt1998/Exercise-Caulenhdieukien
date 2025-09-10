903c2c6f59e4ca4495de71194292e4355711be05
Nguyễn Văn Hùng <quakhuvohinh198@gmail.com>
2025-09-11 06:28:55 +0700

    Program read number

---
+++ ReadNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReadNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program: Read Number");
+            Console.Write("Enter a number: ");
+            int num;
+            num = Int32.Parse(Console.ReadLine());
+
+        }
+    }
+}
